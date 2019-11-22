c23f0fddd89b38733b2f64d69aaf2f8b23863582
JC <carlosestrada482@gmail.com>
2019-11-21 23:19:09 -0600

    Vistas

---
+++ application/views/layout/footer.php
@@ -0,0 +1,25 @@
+</section>
+</main>
+
+
+<!--=============================================
+=            Include JavaScript files           =
+==============================================-->
+
+
+<!-- popper -->
+<script src="<?php echo base_url("resources/js/popper.min.js")?>" ></script>
+
+<!-- Bootstrap V4.3 -->
+<script src="<?php echo base_url("resources/js/bootstrap.min.js")?>" ></script>
+
+<!-- jQuery Custom Content Scroller V3.1.5 -->
+<script src="<?php echo base_url("resources/js/jquery.mCustomScrollbar.concat.min.js")?>" ></script>
+
+<!-- Bootstrap Material Design V4.0 -->
+<script src="<?php echo base_url("resources/js/bootstrap-material-design.min.js")?>" ></script>
+<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+
+<script src="<?php echo base_url("resources/js/main.js")?>" ></script>
+</body>
+</html>
